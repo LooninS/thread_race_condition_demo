5119f9ed844a670f76945b70df9bf31cbcf283ef
Sourish Awasthi <hushed55@proton.me>
2026-06-16 23:52:07 +0530

    Update check.c

--- check.c
+++ check.c
@@ -3,6 +3,7 @@
 #include <string.h>
 
 void error_func(void) {
+  // open shared_string.txt
   FILE *fp = fopen("shared_string.txt", "r");
   if (!fp) {
     printf("Error: file not found\n");
@@ -16,13 +17,14 @@ void error_func(void) {
     line[strcspn(line, "\n")] = '\0';
 
     int found = 0;
+    // compare each line against set of sentences in sentence.c
     for (int i = 0; i < NUM_SENTENCES; i++) {
       if (strcmp(line, sentences[i]) == 0) {
         found = 1;
         break;
       }
     }
-
+    // print error msg if sentence different from sentence.c
     if (!found) {
       printf("ERROR %d: %s\n", lineNum, line);
     }
